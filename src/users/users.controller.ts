b1c71fd9b42c6ff969b5c8055038ad7ff94d0591
d-levchenko <dl.qubb@gmail.com>
2026-08-22 13:38:21 +0300

    pipes validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   constructor() {}
 
-  @Get('{/id}/')
-  public getUsers(@Param('id') id: string) {
-    if (id) {
-      return 'get user request with id: ' + id;
-    }
+  @Get('{/:id}')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(id, limit, page);
     return 'get users request';
   }
 
-  @Post('{/id}/')
+  @Post('{/:id}')
   public postUsers(
     @Body() request: { username: string; email: string; password: string },
   ) {
